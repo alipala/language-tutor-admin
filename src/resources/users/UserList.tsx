5759c17416f865bef35941d71d6199f3126d6ed3
alipala.ist@gmail.com <alipala.ist@gmail.com>
2025-06-28 16:28:09 +0200

    test: DOM manipulation version to bypass console.log stripping

--- src/resources/users/UserList.tsx
+++ src/resources/users/UserList.tsx
@@ -1,9 +1,44 @@
 import React from 'react';
 
-// ULTRA SIMPLE TEST VERSION
+// ULTRA SIMPLE TEST VERSION - NO CONSOLE LOGS
 export const UserList = () => {
-  console.log('🔥 USERLIST COMPONENT LOADED!');
-  alert('🔥 USERLIST COMPONENT LOADED!');
+  // Use DOM manipulation instead of console.log (which gets stripped in production)
+  React.useEffect(() => {
+    // Change document title to prove component loaded
+    document.title = '🔥 USERLIST LOADED! 🔥';
+    
+    // Add a visible element to the page
+    const testDiv = document.createElement('div');
+    testDiv.id = 'userlist-test-indicator';
+    testDiv.style.cssText = `
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: #ff0000;
+      color: white;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 48px;
+      font-weight: bold;
+      z-index: 9999;
+      text-align: center;
+    `;
+    testDiv.innerHTML = '🔥 USERLIST COMPONENT IS WORKING! 🔥<br/>Click anywhere to continue';
+    
+    testDiv.onclick = () => {
+      testDiv.remove();
+    };
+    
+    document.body.appendChild(testDiv);
+    
+    // Also show an alert (should work even in production)
+    setTimeout(() => {
+      alert('🔥 USERLIST COMPONENT LOADED SUCCESSFULLY! 🔥');
+    }, 100);
+  }, []);
   
   return React.createElement('div', {
     style: {
